34a31f4fe635d0f3624543f8bfaeb09ee51bc80d
huyluvnhjm99 <34624305+huyluvnhjm99@users.noreply.github.com>
2020-06-10 08:55:07 +0700

    sadsad

--- lib/models/api.services.dart
+++ lib/models/api.services.dart
@@ -2,7 +2,7 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 class APIServices {
-  static final String personalityTestUrl = 'http://192.168.1.9:5055/api/Personalitytest';
+  static final String personalityTestUrl = 'https://pto.azurewebsites.net/api/v1/Personalitytest';
 
   static Future fectPersonalityTest() async {
     return await http.get(personalityTestUrl);
